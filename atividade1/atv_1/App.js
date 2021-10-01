3bd938517535d5a8466f2e76799009d4fee6c47b
arlegs <86801815+arlegs@users.noreply.github.com>
2021-10-01 20:28:45 -0300

    Atividade 01

--- atividade1/atv_1/App.js
+++ atividade1/atv_1/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Aluno } from './Aluno';
 
 
@@ -9,33 +8,3 @@ export default function App() {
     <Aluno></Aluno>
   );
 }
-
-
-const styles = StyleSheet.create({
-  conteudo: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#292929',
-  },
-  nome: {
-    fontSize: '32px',
-    fontWeight: '700',
-    marginBottom: '10px',
-    color: 'white',
-    letterSpacing: '1.2px',
-  },
-  nome_2: {
-    marginTop: '3px',
-    fontSize: '18px',
-    color: 'white',
-    marginBottom: '3px',
-    
-  },
-  outro_texto: {
-    color: '#c1c1c1',
-    marginTop: '3px',
-    fontSize: '18px',
-  } 
-});
