02879209e93b0fea6ba4c44c5318f6993f5e900a
arlegs <86801815+arlegs@users.noreply.github.com>
2021-10-18 15:39:04 -0300

    atividade 2 - tentativa 3

--- OneDrive/Documentos/PIDM/pidm/atividade2/atv_2/atv2/App.js
+++ OneDrive/Documentos/PIDM/pidm/atividade2/atv_2/atv2/App.js
@@ -21,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f2f2f2',
+    backgroundColor: '#f3f3f3',
     alignItems: 'center',
     justifyContent: 'center',
   },
